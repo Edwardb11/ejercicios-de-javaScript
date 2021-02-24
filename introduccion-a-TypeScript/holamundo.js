83904a74d5f17de5110ae68960addda86b4ffa47
Edward Brito <Edwardbrito11@hotmail.com>
2021-02-24 14:31:34 -0400

    Utilizando el compilador de typeScript tsc y escribiendo codigo ts

---
+++ introduccion-a-TypeScript/holamundo.js
@@ -0,0 +1,5 @@
+function saludar(nombre) {
+    return ("Hola" + nombre);
+}
+var mi_nombre = "Edward Brito";
+var resultado = saludar(mi_nombre);
---
+++ introduccion-a-TypeScript/holamundo.ts
@@ -0,0 +1,6 @@
+function saludar(nombre) {
+    return ("Hola" + nombre )
+}
+
+let mi_nombre="Edward Brito"
+let resultado = saludar(mi_nombre)
