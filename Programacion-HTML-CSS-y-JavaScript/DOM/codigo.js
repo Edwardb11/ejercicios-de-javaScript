1092d1a0fbffacdc143bb5395172469827b3d029
Edward Brito <Edwardbrito11@hotmail.com>
2021-03-07 18:37:35 -0400

    Trabajando con canvas y el DOM

---
+++ Programacion-HTML-CSS-y-JavaScript/DOM/codigo.js
@@ -0,0 +1,8 @@
+var ruta = window.location;
+document.write("Hola mundo") //Documente es un objeto parte de Document Object Model y .wirte es para escribir
+document.write("Estas en :" + ruta) //Windows es toda mi ventana y con location veo la ruta de donde estoy
+
+//Depurar es el proceso de arreglar un codigo 
+console.log("Este es un mensaje secreto.");
+console.log(ruta); //podemos ver como esta escrito un objeto por dentro
+console.log(console.log);
