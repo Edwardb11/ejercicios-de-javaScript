b864f440a4fca5a8a984678553ca0a440eed3dfd
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-08-16 17:03:11 -0400

    Update index.js

--- Condicionales/index.js
+++ Condicionales/index.js
@@ -1,3 +1,7 @@
 var isGoing = false;
 var color = (isGoing ? "green" : "red"); //primer valor antes de los  dos puntos ":" si la condicion es true se evalua y si no pues se evalua lo que esta despues de los dos puntos
 console.log(color);
+
+var adult = true;
+var preorder = true;
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
