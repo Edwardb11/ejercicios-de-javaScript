abc4df49f34819f58ed60a6e210ae32368f6116d
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-08-16 17:02:36 -0400

    Update index.js

--- Condicionales/index.js
+++ Condicionales/index.js
@@ -1,1 +1,3 @@
-
+var isGoing = false;
+var color = (isGoing ? "green" : "red"); //primer valor antes de los  dos puntos ":" si la condicion es true se evalua y si no pues se evalua lo que esta despues de los dos puntos
+console.log(color);
