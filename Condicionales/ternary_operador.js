1ad350d32197988189f7d518501efa1b44dc94f3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-08-16 17:04:23 -0400

    Update and rename index.js to ternary_operador.js

--- Condicionales/ternary_operador.js
+++ Condicionales/ternary_operador.js
@@ -5,3 +5,17 @@ console.log(color);
 var adult = true;
 var preorder = true;
 console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
+
+
+function getFee(isMember) {
+  return (isMember ? '$2.00' : '$10.00');
+}
+
+console.log(getFee(true));
+// expected output: "$2.00"
+
+console.log(getFee(false));
+// expected output: "$10.00"
+
+console.log(getFee(null));
+// expected output: "$10.00"
